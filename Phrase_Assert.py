d3b313dc01eedcf710c8ad5a2ff1752e93a90c3a
El-QA <erkamaletdinova@bk.ru>
2021-07-18 17:09:56 +0400

    Ex14: Формирование фреймворка

--- Phrase_Assert.py
+++
@@ -1,5 +0,0 @@
-class TestPhraseAssert:
-    def test_phrase(self):
-        phrase = input("Set a phrase: ")
-        phrase_length = len(phrase)
-        assert phrase_length < 15, f"Phrase should be less than 15 characters, you phrase has {phrase_length} characters"
--- main.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-redirect_count = len(response.history)
-finally_url = response.url
