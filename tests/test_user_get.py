fd9c44315b430911c41b5061f34312a74d3f38b7
El-QA <erkamaletdinova@bk.ru>
2021-07-19 15:12:05 +0400

    Ex16: Запрос данных другого пользователя

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -30,3 +30,37 @@ class TestUserGet(BaseCase):
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_not_same_user(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response_reg = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response_reg, 200)
+        Assertions.assert_json_has_key(response_reg, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response_login = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response_login, "auth_sid")
+        token = self.get_header(response_login, "x-csrf-token")
+
+        # GET
+        response = MyRequests.get(
+            f"/user/2",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "username")
+        Assertions.assert_json_has_not_key(response, "email")
+        Assertions.assert_json_has_not_key(response, "firstName")
+        Assertions.assert_json_has_not_key(response, "lastName")
